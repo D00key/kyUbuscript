f244e90e97ec70f53f1a220f1deec831165e2f2d
D00key <46851596+D00key@users.noreply.github.com>
2022-07-11 01:14:30 -0700

    Update dookiesetupscript.sh

--- dookiesetupscript.sh
+++ dookiesetupscript.sh
@@ -9,7 +9,8 @@ wget --https-only https://dl.google.com/linux/direct/google-chrome-stable_curren
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt-get --fix-broken install -y && sudo apt install brave-browser -y && sudo aa-enfore /etc/apparmor.d/* && sudo firecfg
+sudo apt-get --fix-broken install -y && sudo apt install brave-browser -y  && sudo firecfg
+sudo aa-enfore /etc/apparmor.d/* 
 curl -L -o VNC.deb https://www.realvnc.com/connect/download/binary/latest/debian/64-bit/
 sudo apt install -y ./VNC.deb
 sudo vnclicense -add HJH22-N2R8C-W6XDA-YCQ66-9D6C3
