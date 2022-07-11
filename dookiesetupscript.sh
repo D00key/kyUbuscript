df169d43d9222f8c6225ad125a10f8b91cb42ef1
D00key <46851596+D00key@users.noreply.github.com>
2022-07-11 00:46:07 -0700

    final fixes

--- dookiesetupscript.sh
+++ dookiesetupscript.sh
@@ -10,9 +10,9 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get --fix-broken install -y && sudo apt install brave-browser -y && sudo aa-enfore /etc/apparmor.d/* && sudo firecfg
+sudo apt install -y ./VNC.deb
+sudo vnclicense -add HJH22-N2R8C-W6XDA-YCQ66-9D6C3
 sudo apt-get install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -L -o VNC.deb https://www.realvnc.com/connect/download/binary/latest/debian/64-bit/
-sudo apt install -y ./VNC.deb
-sudo vnclicense -add HJH22-N2R8C-W6XDA-YCQ66-9D6C3
-rmdir .dookscript
+rmdir .dookscript || exit
